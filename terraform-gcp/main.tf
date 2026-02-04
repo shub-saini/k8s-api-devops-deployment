1b881c122e141350e7a24bba111bbd5d8dc8c3e4
shub-saini <anshsaini04@gmail.com>
2026-02-05 03:54:30 +0530

    terraform fmt -recursive run

--- terraform-gcp/main.tf
+++ terraform-gcp/main.tf
@@ -131,11 +131,11 @@ module "gke" {
 module "gcr" {
   source = "./modules/gcr"
 
-  location            = var.region
-  project_id          = local.project_id
-  repository_id       = "stage-images"
+  location      = var.region
+  project_id    = local.project_id
+  repository_id = "stage-images"
   // it create iam binding for gke's service account, so gke can pull images from gcr repo
-  gke_service_account = module.gke.node_pool_service_accounts 
+  gke_service_account = module.gke.node_pool_service_accounts
 
   depends_on = [google_project_service.apis, module.gke]
 }
