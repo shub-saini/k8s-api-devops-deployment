bf67481230c95cacbc013608a894bbdceb170fc6
shub-saini <anshsaini04@gmail.com>
2026-02-05 10:29:07 +0530

    argocd install from terraform

--- terraform-gcp/provider.tf
+++ terraform-gcp/provider.tf
@@ -18,3 +18,19 @@ provider "google" {
   region = var.region
 
 }
+
+data "google_client_config" "default" {}
+
+provider "kubernetes" {
+  host                   = module.gke.cluster_endpoint
+  cluster_ca_certificate = module.gke.cluster_ca_certificate
+  token                  = data.google_client_config.default.access_token
+}
+
+provider "helm" {
+  kubernetes = {
+    host                   = module.gke.cluster_endpoint
+    cluster_ca_certificate = module.gke.cluster_ca_certificate
+    token                  = data.google_client_config.default.access_token
+  }
+}
